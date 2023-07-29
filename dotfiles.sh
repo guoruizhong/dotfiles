5bb70ec33280021b3608733e6553828e0e1ba975
guoruizhong <grgrzhong@gmail.com>
2023-07-29 11:32:57 +0800

    update tmux configuration

---
+++ dotfiles.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+# create a new repository on the command line
+# echo "# update dotfiles" >> README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git branch -M main
+# git remote add origin https://github.com/guoruizhong/dotfiles.git
+# git push -u origin main
+# git init
+
+git add .
+git commit -m "update tmux configuration"
+git push -u origin main
